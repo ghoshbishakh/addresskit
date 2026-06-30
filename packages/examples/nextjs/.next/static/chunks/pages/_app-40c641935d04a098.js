67a56c6f6fa8781638ae601ce6c41136ae063aae
Bishakh Ghosh <ghoshbishakh@gmail.com>
2026-06-30 10:34:08 +0530

    chore: add .next to gitignore, remove tracked build artifacts

--- packages/examples/nextjs/.next/static/chunks/pages/_app-40c641935d04a098.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{1891:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(8671)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(1891),n(5095))),_N_E=_.O()}]);
